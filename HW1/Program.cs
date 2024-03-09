ba4d4860fd966cfd6d3ce1ba00e03892e80026d6
Arturs <janvaris91@gmail.com>
2024-03-09 10:03:19 +0100

    Add homework to HM1

---
+++ HW1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Homework. Create 4 varaible as whole number and make formula (a * b) / (c + d)
+*/
+// This code will only show result as whole number without decimal point
+int a = 6;
+int b = 4;
+int c = 3;
+int d = 2;
+
+int result = (a * b) / (c + d);
+Console.WriteLine(result);
+
+// This code will show result with decimal point
+double a1 = 6;
+int b1 = 4;
+int c1 = 3;
+int d1 = 2;
+
+double result2 = (a1 * b1) / (c1 + d1);
+Console.WriteLine(result2);
