0c3a664c1c97b24c54250ed40cb92aa25f7a76e2
Arturs <janvaris91@gmail.com>
2024-03-09 10:55:40 +0100

    added extra functions to Task5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,8 +1,10 @@
-﻿int n = 10;
+﻿int n = 1000;
 int i = 1;
 
 while (i <= n)
 {
-    Console.WriteLine(i);
+   // Console.WriteLine(i);
+    Console.Write(i);
+    Console.Write(' ');
     i = i + 1;
 }
