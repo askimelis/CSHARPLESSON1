00db2cb46a1a165a2b3d5aac8fdfada4ba7b9eba
Arturs <janvaris91@gmail.com>
2024-03-09 09:20:37 +0100

    add few variables Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,16 @@
-﻿Console.WriteLine("Hello, Arturs!");
+﻿// Single line comment
+// Console.WriteLine("Hello, Arturs!");
+/*
+Multi line comment
+*/
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
 
