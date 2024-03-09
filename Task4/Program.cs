784e35b6debe903957540f477c79702eb87a9b2a
Arturs <janvaris91@gmail.com>
2024-03-09 10:30:56 +0100

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = 4;
+int b = 5000000;
+int c = 16;
+int d = 99;
+int e = -7;
+
+int max = a;
+
+if (b > max) 
+{
+    max = b;
+}
+
+if (c > max) 
+{
+    max = c;
+}
+
+if (d > max) 
+{
+    max = d;
+}
+
+if (e > max) 
+{
+    max = e;
+}
+
+Console.WriteLine(max);
